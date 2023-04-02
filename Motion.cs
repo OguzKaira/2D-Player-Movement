e98c3309bdec5f836fcd9a85df532a4707236a41
Oğuz Kayra Büyükyıldırım <74321096+OguzKaira@users.noreply.github.com>
2023-04-02 22:55:45 +0300

    Create Motion.cs

---
+++ Motion.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
